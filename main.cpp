f9d895560a4828357905f370ffb49abb463e85f4
Marcelo Maia Sobral <msobral@ifsc.edu.br>
2020-11-27 17:02:03 -0300

    Mensagem múltipla com numeração

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     for (int j=0; j < 5; j++) {
-        cout << "Hello, World!" << endl;
+        cout << "Hello, World: " << j << endl;
     }
 
     return 0;
